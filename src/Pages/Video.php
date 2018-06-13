acfa545b1d739b080edeb807d4c1f279843bcde8
Nic Horstmeier <nhorstmeier@dynamicagency.com>
2018-06-13 10:54:14 -0500

    UPDATE additional SS4 adjustments

---
+++ src/Pages/Video.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Dynamic\HTML5Video\Pages;
+
+use Page;
+
+use SilverStripe\Assets\File;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\TextField;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Forms\RequiredFields;
+use SilverStripe\View\Requirements;
+use PageController;
+
+
+/**
+ * Class Video
+ * @package Dynamic\HTML5Video\Pages
+ */
+class Video extends Page
+{
+	/**
+	 * @var array
+	 */
+	private static $db = [
+		'Time' => 'Varchar(100)',
+	];
+
+	/**
+	 * @var array
+	 */
+	private static $has_one = [
+		'MP4Video' => File::class,
+		'OggVideo' => File::class,
+		'WebMVideo' => File::class,
+		'Image' => Image::class,
+	];
+
+	/**
+	 * @var string
+	 */
+	private static $singular_name = 'HTML5 Video';
+
+	/**
+	 * @var string
+	 */
+	private static $plural_name = 'HTML5 Vidoes';
+
+	/**
+	 * @var string
+	 */
+	private static $description = 'Single Video Detail Page';
+
+	/**
+	 * @var string
+	 */
+	private static $table_name = 'Video';
+
+	/**
+	 * Sets the ShowInMenus field to false.
+	 */
+	public function populateDefaults()
+	{
+		$this->ShowInMenus = 0;
+		parent::populateDefaults();
+	}
+
+	/**
+	 * @return \SilverStripe\Forms\FieldList
+	 */
+	public function getCMSFields()
+	{
+		$fields = parent::getCMSFields();
+
+		$fields->dataFieldByName('Title')->setTitle('Video Title');
+		$fields->dataFieldByName('Content')->setTitle('Video Description');
+
+		$fields->insertBefore(
+			TextField::create('Time', 'Video Duration')
+				->setDescription('ex. mm:ss'),
+			'Content'
+		);
+
+		// poster
+		$PosterField = UploadField::create(Image::class, 'Poster Image')
+			->setFolderName('Uploads/Video/Images')
+			->setConfig('allowedMaxFileNumber', 1)
+			->setDescription('Preview image for the video.');
+		$PosterField->allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+		$PosterField->getValidator()->setAllowedMaxFileSize(VIDEO_IMAGE_FILE_SIZE_LIMIT);
+
+		$fields->insertBefore(
+			$PosterField,
+			'Content'
+		);
+
+		$MP4Field = new UploadField('MP4Video');
+		$MP4Field
+			->setTitle('MP4 Video')
+			->setFolderName('Uploads/Video/MP4Video')
+			->setConfig('allowedMaxFileNumber', 1)
+			->setDescription('Required. Format compatible with most browsers.');
+		$MP4Field->getValidator()->setAllowedExtensions(['mp4', 'm4v']);
+		$MP4Field->getValidator()->setAllowedMaxFileSize(VIDEO_FILE_SIZE_LIMIT);
+
+		$OggField = UploadField::create('OggVideo');
+		$OggField
+			->setTitle('Ogg Video')
+			->setFolderName('Uploads/Video/OggVideo')
+			->setConfig('allowedMaxFileNumber', 1)
+			->setDescription('Optional. Format compatible with FireFox.');
+		$OggField->getValidator()->setAllowedExtensions(['ogv', 'ogg']);
+		$OggField->getValidator()->setAllowedMaxFileSize(VIDEO_FILE_SIZE_LIMIT);
+
+		$WebMField = UploadField::create('WebMVideo');
+		$WebMField
+			->setTitle('WebM Video')
+			->setFolderName('Uploads/Video/WebMVideo')
+			->setConfig('allowedMaxFileNumber', 1)
+			->setDescription('Optional. Format compatible with Chrome.');
+		$WebMField->getValidator()->setAllowedExtensions(['webm']);
+		$WebMField->getValidator()->setAllowedMaxFileSize(VIDEO_FILE_SIZE_LIMIT);
+
+		$fields->addFieldsToTab('Root.Video', [
+			$MP4Field,
+			$WebMField,
+			$OggField,
+		]);
+
+		return $fields;
+	}
+
+	/**
+	 * This function allows the validation of record data
+	 * on Save.
+	 *
+	 * @return RequiredFields
+	 */
+	public function getCMSValidator()
+	{
+		return new RequiredFields(['MP4Video']);
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function getRelatedVideos()
+	{
+		if ($this->Parent() && $this->Parent()->ClassName == VideoGroup::class) {
+			return $this->Parent()->getVideoList()->exclude('ID', $this->ID);
+		}
+
+		return false;
+	}
+}
---
+++ src/Pages/VideoGroupController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Dynamic\HTML5Video\Pages;
+
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\ORM\ArrayList;
+use SilverStripe\ORM\FieldType\DBHTMLText;
+use SilverStripe\ORM\PaginatedList;
+
+/**
+ * Class VideoGroup_Controller
+ * @package Dynamic\HTML5Video\Pages
+ */
+class VideoGroup_Controller extends \PageController
+{
+	/**
+	 * @var array
+	 */
+	private static $allowed_actions = [
+		'index',
+	];
+
+	/**
+	 *
+	 */
+	public function init()
+	{
+		parent::init();
+
+	}
+
+	/**
+	 * @param HTTPRequest $request
+	 * @return array|DBHTMLText
+	 */
+	public function index(HTTPRequest $request)
+	{
+
+		$videos = PaginatedList::create(
+			$this->data()->getVideoList(),
+			$request
+		)->setPageLength(Config::inst()->get(VideoGroup::class, 'page_length'))
+			->setPaginationGetVar(Config::inst()->get(VideoGroup::class, 'pagination_get_var'));
+
+		$data = [
+			'VideoList' => $videos,
+		];
+
+		if ($request->isAjax()) {
+			return $this->customise($data)->renderWith('VideoList');
+		}
+
+		return $data;
+	}
+
+	/**
+	 * Method called from the layout that can be passed
+	 * a boolean variable to override the default behavior
+	 * of getChildGroups().
+	 *
+	 * @param bool $all
+	 *
+	 * @return ArrayList
+	 */
+	public function SubGroups($all = false)
+	{
+		return $this->data()->getChildGroups($all);
+	}
+
+
+}
