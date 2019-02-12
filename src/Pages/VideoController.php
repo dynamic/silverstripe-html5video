9484ba319552c449479fdf4b1adb585c0a5450bb
Jason Korth <jkorth@dynamicagency.com>
2019-02-12 11:31:24 -0600

    adjust requirements (#49)
    * adjust requirements
    
    fixes #48
    
    * remove init function

--- src/Pages/VideoController.php
+++ src/Pages/VideoController.php
@@ -10,11 +10,4 @@ use SilverStripe\View\Requirements;
  */
 class VideoController extends \PageController
 {
-    public function init()
-    {
-        parent::init();
-
-        Requirements::css(VIDEO_DIR . '/thirdparty/video-js/video-js.min.css');
-        Requirements::javascript(VIDEO_DIR . '/thirdparty/video-js/video.min.js');
-    }
 }
